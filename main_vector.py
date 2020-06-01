e1cd530e6f99e8b9d5a9a26819150928a2d359ce
huanshenyi <txy1226052>
2020-06-01 20:58:49 +0900

    vectorのループ、足し算、引き算、掛け算、プラス値、マイナス値取得できる

--- main_vector.py
+++ main_vector.py
@@ -6,3 +6,12 @@ if __name__ == "__main__":
     print(vec) # (5, 2)
     print(len(vec)) # 2
     print(vec[0]) # 5
+
+    vec2 = Vector([3, 1])
+    print(vec + vec2)  # (8, 6)
+    print(vec - vec2)  # (2, 1)
+    print(vec * 2)     # (10, 4)
+    print(2 * vec)     # (10, 4)
+
+    print(-vec)        # (-5, -2)
+    print(+vec)        # (5, 2)
--- useVector/Vector.py
+++ useVector/Vector.py
@@ -1,7 +1,7 @@
 class Vector:
-    def __init__(self, list):
+    def __init__(self, lst):
         # vectorの内容を保存する
-        self._value = list
+        self._value = list(lst)
 
     def __getitem__(self, index):
         """Vectorの何個目の要素"""
@@ -18,3 +18,65 @@ class Vector:
     def __str__(self):
         # print(u) > (x, x)
         return f"({', '.join(str(e) for e in self._value)})"
+
+    def __iter__(self):
+        """
+        vectorループ用
+        :return:
+        """
+        return self._value.__iter__()
+
+    def __add__(self, other):
+        """
+        Vectorの足し算
+        other > 別のVector
+        """
+        # 現在のVectorとターゲットVectorの長さが一致することが前提とする
+        assert len(self) == len(other), \
+            "Error in adding . Length of vectors must be same"
+        return Vector([a + b for a, b in zip(self, other)])
+
+    def __sub__(self, other):
+        """
+        Vectorの引き算
+        :param other:
+        :return:
+        """
+        # 現在のVectorとターゲットVectorの長さが一致することが前提とする
+        assert len(self) == len(other), \
+            "Error in adding . Length of vectors must be same"
+        return Vector([a - b for a, b in zip(self, other)])
+
+    def __mul__(self, k):
+        """
+        掛け算 self * k
+        :param k:
+        :return:
+        """
+        return Vector([k * e for e in self])
+
+    def __rmul__(self, k):
+        """
+        掛け算  k * self right
+        :param k:
+        :return:
+        """
+        return Vector([k * e for e in self])
+
+    def __pos__(self):
+        """
+        Vectorのプラス値取得
+        :return:
+        """
+        return 1 * self
+
+    def __neg__(self):
+        """
+        Vectorのマイナス値を取得
+        :return:
+        """
+        return -1 * self
+
+
+
+
