649de449d5205bebb504bbae9ce479986dc6f393
huanshenyi <txy1226052@gmail.com>
2020-06-24 13:32:16 +0900

    マトリクスのclassが完成

--- useVector/Matrix.py
+++ useVector/Matrix.py
@@ -5,6 +5,16 @@ class Matrix:
     def __init__(self, list2d):
         self._value = [row[:] for row in list2d]
 
+    @classmethod
+    def zero(cls, r, c):
+        """
+        r行c列の0マトリクスを返す
+        :param r:
+        :param c:
+        :return:
+        """
+        return cls([[0] * c for _ in range(r)])
+
     def __add__(self, another):
         """
         マトリクスの足し算
@@ -24,6 +34,44 @@ class Matrix:
         assert self.shape() == another.shape(), \
             "Error in adding. Shape of matrix must be same."
         return Matrix([[a - b for a, b in zip(self.row_vector(i), another.row_vector(i))]] for i in range(len(self)))
+
+    def __mul__(self, k):
+        """
+        マトリクスの掛け算
+        :param k:
+        :return:
+        """
+        return Matrix([e * k for e in self.row_vector(i)] for i in range(len(self)))
+
+    def __rmul__(self, k):
+        """
+        マトリクスの掛け算
+        :param k:
+        :return:
+        """
+        return self * k
+
+    def __truediv__(self, k):
+        """
+        マトリクスの引き算
+        :param k:
+        :return:
+        """
+        return (1/k) * self
+
+    def __pos__(self):
+        """
+        マトリクスのプラス値を返す
+        :return:
+        """
+        return 1*self
+
+    def __neg__(self):
+        """
+        マトリクスのマイナス値
+        :return:
+        """
+        return -1 * self
 
     def row_vector(self, index):
         """
