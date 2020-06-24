ff87a4768687fd6ef7e15a02ed0b7f16cd477f67
huanshenyi <txy1226052@gmail.com>
2020-06-24 13:33:08 +0900

    マトリクスの計算例

--- main_matrix.py
+++ main_matrix.py
@@ -10,3 +10,10 @@ if __name__ == "__main__":
     print("len(matrix)={}".format(len(matrix))) # 2
     print("matrix[0][0]={}".format(matrix[0, 0])) # matrix[0][0]=1
     print("matrix")
+
+    matrix2 = Matrix([[5, 6], [7, 8]])
+    print("add:{}".format(matrix + matrix2)) # Matrix([[[6, 8]], [[10, 12]]])
+    print("sub:{}".format(matrix - matrix2)) # Matrix([[[-4, -4]], [[-4, -4]]])
+    print("scalar-mul:{}".format(matrix*2)) # scalar-mul:Matrix([[2, 4], [6, 8]])
+    print("scalar-mul:{}".format(2 * matrix)) # scalar-mul:Matrix([[2, 4], [6, 8]])
+    print(Matrix.zero(2, 3)) # Matrix([[0, 0, 0], [0, 0, 0]])
